9a51989cbbb374db5ac3ed3e86e971c0ae1409c6
aGhouzlane <ahmedghouzlane@gmail.com>
2021-05-14 14:37:05 -0400

    Add bussiness logic to order file

---
+++ VendorOrderTracker/Models/Order.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace VendorOrderTracker.Models
+{
+  public class Order
+  {
+    public int Id { get; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Quantity { get; set; }
+    public float price { get; set; }
+    public DateTime DateTime { get; set; }
+    private static List<Order> orders = new List<Order> { };
+    public Order(string title, string description, float price)
+    {
+      Title = title;
+      Description = description;
+      Price = price;
+      orders.Add(this);
+      Id = orders.Count;
+    }
+    public static List<Item> GetAll()
+    {
+      return orders;
+    }
+
+    public static void ClearAll()
+    {
+      orders.Clear();
+    }
+
+    public static Order Find(int searchId)
+    {
+      return orders[searchId - 1];
+    }
+  }
+}
